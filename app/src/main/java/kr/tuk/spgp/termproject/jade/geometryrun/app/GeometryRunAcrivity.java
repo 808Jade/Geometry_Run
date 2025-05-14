c7958d44643050c8a658179d8452c6a6827fcb81
MainPC <129085079+808Jade@users.noreply.github.com>
2025-05-14 21:26:30 +0900

    HorzScrollBackground 추가

--- app/src/main/java/kr/tuk/spgp/termproject/jade/geometryrun/app/GeometryRunAcrivity.java
+++ app/src/main/java/kr/tuk/spgp/termproject/jade/geometryrun/app/GeometryRunAcrivity.java
@@ -7,6 +7,7 @@ import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics;
 import kr.tuk.spgp.termproject.jade.geometryrun.BuildConfig;
+import kr.tuk.spgp.termproject.jade.geometryrun.game.MainScene;
 
 public class GeometryRunAcrivity extends GameActivity {
     @Override
@@ -15,6 +16,6 @@ public class GeometryRunAcrivity extends GameActivity {
         Metrics.setGameSize(1600, 900);
 
         super.onCreate(savedInstanceState);
-        new Scene().push();
+        new MainScene().push();
     }
 }
--- app/src/main/java/kr/tuk/spgp/termproject/jade/geometryrun/game/MainScene.java
+++ app/src/main/java/kr/tuk/spgp/termproject/jade/geometryrun/game/MainScene.java
@@ -1,4 +1,21 @@
 package kr.tuk.spgp.termproject.jade.geometryrun.game;
 
-public class MainScene {
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.HorzScrollBackground;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.VertScrollBackground;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
+import kr.tuk.spgp.termproject.jade.geometryrun.R;
+
+public class MainScene extends Scene {
+    public enum Layer {
+        bg; // enum case 외에 다른 멤버가 있으므로, 마지막에 semi-colon (;) 을 써야 한다.
+        public static final int COUNT = values().length; // COUNT 의 정의는 항상 복사해서 붙여넣자.
+    }
+
+    public MainScene() {
+        initLayers(Layer.COUNT);
+
+        add(Layer.bg, new HorzScrollBackground(R.mipmap.background_basic, 100f));
+//        add(Layer.bg, new HorzScrollBackground(R.mipmap.background_floor, 200f));
+//        add(Layer.bg, new VertScrollBackground(R.mipmap.background_floor_gradation, 300f));
+    }
 }
