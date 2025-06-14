64ab06991ce2b90a96b83c3690e5e9e5595d723f
MainPC <129085079+808Jade@users.noreply.github.com>
2025-06-14 18:34:07 +0900

    floorbox Type있게끔 (일단 한 개)

--- app/src/main/java/kr/tuk/spgp/termproject/jade/geometryrun/game/FloorBox.java
+++ app/src/main/java/kr/tuk/spgp/termproject/jade/geometryrun/game/FloorBox.java
@@ -1,19 +1,37 @@
 package kr.tuk.spgp.termproject.jade.geometryrun.game;
 
+import android.graphics.Bitmap;
 import android.graphics.RectF;
 
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.res.BitmapPool;
+import kr.tuk.spgp.termproject.jade.geometryrun.R;
+
 public class FloorBox extends MapObject{
-    public FloorBox() {
-        // super(R.mipmap.floorbox);
+    public enum Type {
+        T_10x2, T_2x2, T_3x1;
+        Bitmap bitmap() { return BitmapPool.get(resId()); }
+        int resId() { return resIds[this.ordinal()]; }
+        int width() { return sizes[this.ordinal()][0]; }
+        int height() { return sizes[this.ordinal()][1]; }
+        static final int[] resIds = {
+                R.mipmap.floorbox
+        };
+        static final int[][] sizes = {
+                { 100, 100 }
+        };
     }
+    public FloorBox() { }
 
-    private FloorBox init(float left, float top) {
-
+    private FloorBox init(Type type,float left, float top) {
+        bitmap = type.bitmap();
+        width = 100;
+        height = 100;
+        dstRect.set(left, top, left + width, top + height);
         return this;
     }
 
-    public static FloorBox get(float left, float top) {
-        return new FloorBox().init(left, top);
+    public static FloorBox get(Type type, float left, float top) {
+        return new FloorBox().init(type, left, top);
     }
 
     @Override
